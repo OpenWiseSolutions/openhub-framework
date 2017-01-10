e7ac469eb400ec0d402417078da1a3a74f6b38f2
Janci Krajnak <jan.krajnak@email.cz>
2017-01-10 15:33:34 +0100

    Replaced browserHistory with hashHistory

--- admin-console-client/src/containers/AppContainer.js
+++ admin-console-client/src/containers/AppContainer.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { browserHistory, Router } from 'react-router'
+import { Router, hashHistory } from 'react-router'
 import { Provider } from 'react-redux'
 
 class AppContainer extends Component {
@@ -18,7 +18,7 @@ class AppContainer extends Component {
     return (
       <Provider store={store}>
         <div style={{ height: '100%' }}>
-          <Router history={browserHistory} children={routes} />
+          <Router history={hashHistory} children={routes} />
         </div>
       </Provider>
     )
