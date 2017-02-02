198e5842d55921cc23a81f20df989b145a8dc242
Tomas Hanus <tomas.hanus@openwise.cz>
2017-02-02 07:52:23 +0100

    Changed login and logout URLs to correct server authentication handling.
    Issue: OHFJIRA-40

--- admin-console/server/main.js
+++ admin-console/server/main.js
@@ -35,8 +35,8 @@ if (project.env === 'development') {
   app.use(require('webpack-hot-middleware')(compiler))
 
   // Mock server & proxy
-  app.post('/login', (req, res) => { res.send('OK') })
-  app.get('/logout', (req, res) => { res.send('OK') })
+  app.post('/web/admin/login', (req, res) => { res.send('OK') })
+  app.get('/web/admin/logout', (req, res) => { res.send('OK') })
   app.use(project.mock_server_prefix, proxy(project.mock_server_host))
 
   // Serve static assets from ~/public since Webpack is unaware of
