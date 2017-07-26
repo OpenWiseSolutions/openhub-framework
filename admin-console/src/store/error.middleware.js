b031cb8f5a6ac4d9c3d063092fd4c25af086ad98
Janci Krajnak <jan.krajnak@email.cz>
2017-07-26 20:16:46 +0200

    Fixed 401 handler

--- admin-console/src/store/error.middleware.js
+++ admin-console/src/store/error.middleware.js
@@ -5,14 +5,17 @@ import { FETCH_ERROR } from '../services/api.module'
 
 export default (store) => (next) => (action) => {
   if (action.type === FETCH_ERROR) {
-    const status = path(['payload', 'response', 'status'], action)
+    const errorResponse = path(['payload', 'response', 'response'], action)
+    const status = errorResponse.status
     const errorMessage = path(['payload', 'response', 'message'], action)
     const errorCode = path(['payload', 'response', 'errorCode'], action)
+
     if (status === 401) {
       store.dispatch(logoutUser())
       store.dispatch(toggleLoginModal())
       return
     }
+
     if (status === 404) {
       toastr.error(`404 Error`, 'Not Found')
     }
