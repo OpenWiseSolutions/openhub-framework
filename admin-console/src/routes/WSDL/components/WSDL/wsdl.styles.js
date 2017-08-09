f2b6f8f6f611294b90b91dd5017adab02cbaddbf
Janci Krajnak <jan.krajnak@email.cz>
2017-08-09 11:07:51 +0200

    Refactored WSDL

--- admin-console/src/routes/WSDL/components/WSDL/wsdl.styles.js
+++ admin-console/src/routes/WSDL/components/WSDL/wsdl.styles.js
@@ -1,23 +1,9 @@
-import { primaryColor } from '../../../../styles/colors'
-import styles from '../../../../styles/styles'
 import { gap } from '../../../../styles/constants'
 
 export default {
-  ...styles,
   item: {
     paddingBottom: gap,
     marginTop: gap,
     boxShadow: '0 2px 7px -1px silver'
-  },
-  content: {
-    paddingLeft: gap,
-    paddingRight: gap
-  },
-  title: {
-    paddingLeft: gap,
-    paddingRight: gap,
-    paddingTop: gap,
-    paddingBottom: gap,
-    backgroundColor: primaryColor
   }
 }
