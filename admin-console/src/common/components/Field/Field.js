307b0883415d3a18d2ba4977978ec8ec26d72160
Janci Krajnak <jan.krajnak@email.cz>
2017-08-10 16:57:15 +0200

    Components cleanup

--- admin-console/src/common/components/Field/Field.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-import { Field } from 'valid-react-form'
-import styles from './field.styles.js'
-import { withContext } from 'recompose'
-
-export default withContext({
-  styles: PropTypes.object },
-  () => ({ styles })
-)(Field)
--- admin-console/src/common/components/index.js
+++ admin-console/src/common/components/index.js
@@ -1,1 +1,2 @@
 export { default as LoginCard } from './LoginCard/LoginCard'
+export { default as Status } from './Status/Status'
