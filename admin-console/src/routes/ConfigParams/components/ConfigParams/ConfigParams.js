7030c85941478511939d5745d5cbecb09f9906b9
Janci Krajnak <jan.krajnak@email.cz>
2017-08-22 09:09:00 +0200

    ERR-19 Fixed config params table

--- admin-console/src/routes/ConfigParams/components/ConfigParams/ConfigParams.js
+++ admin-console/src/routes/ConfigParams/components/ConfigParams/ConfigParams.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Radium from 'radium'
 import Card from 'react-md/lib/Cards/Card'
-import CardTitle from 'react-md/lib/Cards/CardTitle'
 import DataTable from 'react-md/lib/DataTables/DataTable'
 import TableHeader from 'react-md/lib/DataTables/TableHeader'
 import TableBody from 'react-md/lib/DataTables/TableBody'
@@ -35,41 +34,41 @@ class ConfigParams extends Component {
 
     const categories = configParams.reduce((acc, item) => {
       if (!acc[item.categoryCode]) acc[item.categoryCode] = []
-      acc[item.categoryCode] = [ ...acc[item.categoryCode], item ]
+      acc[item.categoryCode] = [...acc[item.categoryCode], item]
       return acc
     }, {})
 
     return (
       <div style={styles.main} >
-        { !!paramDetail && <EditParamModal
+        {!!paramDetail && <EditParamModal
           updateError={updateError}
           updating={updating}
           close={closeParam}
           updateParam={updateParam}
           data={paramDetail}
-          isOpen={!!paramDetail} /> }
-        { Object.keys(categories).map((cat) => (
-          <Card style={styles.card} key={cat}>
-            <CardTitle title={cat} />
-            <DataTable plain>
-              <TableHeader >
-                <TableRow >
-                  <TableColumn >Code</TableColumn >
-                  <TableColumn >Current Value</TableColumn >
-                  <TableColumn >Default Value</TableColumn >
-                  <TableColumn >Description</TableColumn >
-                  <TableColumn >Action</TableColumn >
-                </TableRow >
-              </TableHeader >
-              <TableBody >
-                { categories[cat].map((row, count) => (
+          isOpen={!!paramDetail} />}
+        <Card style={styles.card} >
+          <DataTable plain >
+            <TableHeader >
+              <TableRow >
+                <TableColumn >Code</TableColumn >
+                <TableColumn >Current Value</TableColumn >
+                <TableColumn >Default Value</TableColumn >
+                <TableColumn >Description</TableColumn >
+                <TableColumn >Action</TableColumn >
+              </TableRow >
+            </TableHeader >
+            {Object.keys(categories).map((cat) => (
+              <TableBody key={cat} >
+                <h2 style={{ padding: '20px' }} >{cat}</h2 >
+                {categories[cat].map((row, count) => (
                   <ParamRow count={count} openParam={openParam} key={row.id} data={row} />
                 ))}
               </TableBody >
-            </DataTable >
-          </Card>
-        ))}
-      </div>
+            ))}
+          </DataTable >
+        </Card >
+      </div >
     )
   }
 }
