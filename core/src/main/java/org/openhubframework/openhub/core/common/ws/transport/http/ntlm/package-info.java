52601d82f82a32ad10a8a44e3295e927cfe34211
Karel Kovářík <kovarikkarel@gmail.com>
2018-11-05 12:30:29 +0100

    [OHFJIRA-94] : Ntlm messageSender & jcifs dependency removed

--- core/src/main/java/org/openhubframework/openhub/core/common/ws/transport/http/ntlm/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * NTLM extension of HttpClient.
- *
- * @see <a href="http://hc.apache.org/httpcomponents-client-ga/ntlm.html">NTLM support in HttpClient</a>
- */
-package org.openhubframework.openhub.core.common.ws.transport.http.ntlm;
